678511711f224113f43bc720f429fdecbcdfc520
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2025-10-29 13:32:09 +0400

    Create gameClasses.h

---
+++ RPG Game/gameClasses.h
@@ -0,0 +1,175 @@
+#pragma once
+#include "BaseClass.h"
+
+//наследование - использование доступных свойств
+// и методов класса родителям (parent), классом наследником(child)
+class Warrior : public virtual Npc //наследование с модификатором доступа public
+{
+protected:  //модификатор 0 (приватный - защищенный, доступ к полям, только внутри класса)
+    unsigned short strenght{ 31 };
+    string weapons[4] = { "кастет", "дубинка", "клинок", "меч" };
+public:
+    //конструктор - метод, который вызывается в момент создания экземпляра
+    //класса (вручную вызвать в основном потоке программы не можем)
+
+    Warrior() //конструктор по умолчанию, когда нет аргументов
+    {
+        name = "воин";
+        health = 35;
+        damage = 10;
+    }
+    //кастомный конструктор
+    Warrior(string name, unsigned int health, float damage)
+    {
+        cout << "кастомный конструктор война" << endl;
+        this->name = name;
+        this->health = health;
+        this->damage = damage;
+    }
+
+    void GetWeapons()
+    {
+        cout << name << " взял в руки " << weapons[lvl - 1];
+    }
+    void GetInfo() override  //полиморфизм (перегрузка для метода)
+    {
+        Npc::GetInfo();
+        cout << "Сила - " << strenght << endl;
+        cout << "Доступное оружие - ";
+        for (int i = 0; i < lvl; i++)
+        {
+            cout << weapons[i] << endl;
+        }
+    }
+    void Create() override
+    {
+        cout << "Вы создали война" << endl;
+        cout << "Введите имя персонажа\t";
+        cin >> name;
+        GetInfo();
+        GetWeapons();
+    }
+    //перегрузка операторов
+    //перегрузка оператора сравнения (==)
+    bool operator == (const Warrior& warrior) const
+    {
+        this->name = npc.name;
+    }
+
+        bool Save(override)
+    {
+        if (Npc::Save)
+        {
+            ofstream saveSystem("save.bin" | ios::app);
+            if (saveSystem.is_open())
+            {
+                saveSystem.write(reinterpret_cast<const char*>(&strenght), sizeof(strenght));
+                for (int i = 0; i < 4; i++)
+                    saveSystem.write(reinterpret_cast<const char*>(&weapons[i]), sizeof(weapons[i]));
+            }
+            saveSystem.close();
+            return true;
+        }
+        else
+        {
+            cout << "сохранение не удалось\nПопроуйбте позже\n";
+            return false
+        }
+    }
+        Warrior Load()
+        {
+            ifstream loadSystem("save.bin", ios::binary);
+            Warrior 
+        }
+
+
+    //деструктор - метод, который вызывается автоматически при высвобождении памяти
+    //при окончании работы с экземпляром класса (нельзя вызвать вручную)
+    ~Warrior() //деструктор всегда без аргументов
+    {
+        cout << name << " пал смертью храбрых" << endl;
+    }
+};
+
+
+//virtual - создает виртуализацию методов, классов
+//при этом сам класс повторно не создается
+class Wizard : public virtual Npc
+{
+protected:
+    unsigned short intellect = 27;
+    string spell[4] = { "вспышка", "магисческая стрела", "огненный шар", "метеоритный дождь" };
+public:
+    Wizard()
+    {
+        name = "волшебник";
+        health = 23;
+        damage = 15;
+    }
+    Wizard(string name, unsigned int health, float damage)
+    {
+        cout << "кастомный конструктор волшебника" << endl;
+        this->name = name;
+        this->health = health;
+        this->damage = damage;
+    }
+    void GetInfo() override  //полиморфизм (перегрузка для метода)
+    {
+        Npc::GetInfo();
+        cout << "Интеллект - " << intellect << endl;
+        cout << "Доступные заклинания в книге заклинаний - ";
+        for (int i = 0; i < lvl; i++)
+        {
+            cout << spell[i] << endl;
+        }
+    }
+    void CastSpell()
+    {
+        cout << name << " применяет " << spell[lvl - 1] << endl;
+    }
+    void Create() override
+    {
+        cout << "Вы создали волшебника" << endl;
+        cout << "Введите имя персонажа\t";
+        cin >> name;
+        GetInfo();
+        CastSpell();
+    }
+    ~Wizard() //деструктор всегда без аргументов
+    {
+        cout << name << " испустил дух" << endl;
+    }
+};
+
+//множественное наследование
+class Paladin : public Warrior, public Wizard
+    //следующий родительственный класс добавляется через запятую
+{
+public:
+    Paladin()
+    {
+        name = "паладин";
+        health = 25;
+        damage = 12;
+        strenght = 27;
+    }
+    void GetInfo() override
+    {
+        Warrior::GetInfo();
+        cout << "Интеллект - " << intellect << endl;
+        cout << "Доступные заклинания в книге заклинаний - ";
+        for (int i = 0; i < lvl; i++)
+        {
+            cout << spell[i] << endl;
+        }
+    }
+    void Create() override
+    {
+        cout << "Вы создали паладина" << endl;
+        cout << "Введите имя персонажа\t";
+        cin >> name;
+        GetInfo();
+        CastSpell();
+        GetWeapons();
+    }
+};
