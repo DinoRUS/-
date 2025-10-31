84ed04ee40394255af2d68c00083c093c393bcf5
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2025-10-31 13:31:17 +0400

    Update main.cpp

--- RPG Game/main.cpp
+++ RPG Game/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "windows.h"
-#include "mainClasses.h"
-#include "mainClasse.h"
+#include "gameClasses.h"
+#include "mainClass.h"
+#include "Evil.h" 
 
 using namespace std;
 
@@ -20,84 +21,84 @@ using namespace std;
 
 int main()
 {
-  setlocale(LC_ALL, "Rus");
-  SetConsoleCP(1251);
-  SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-  Warrior* warrior = new Warrior();
-  Warrior* warrior2 = new Warrior();
-  cout << (*warrior == *warrior2) << endl;
+    Warrior* warrior = new Warrior();
+    Warrior* warrior2 = new Warrior();
+    cout << (*warrior == *warrior2) << endl;
 
-  Wizard* wizard = new Wizard();
-  Paladin* paladin = new Paladin();
-  Player* player = new Player();
+    Wizard* wizard = new Wizard();
+    Paladin* paladin = new Paladin();
+    Player* player = new Player();
 
-  cout << "Привет, путник\nПрисядь у костра и расскажи о себе\n";
-  cout << "Ты впервые тут? (1 - новый персонаж, 2 - загрузить)\n";
-  unsigned short choise = 1;
-  //создать уникальную функцию для проверки условия выбора
-  cin >> choise;
-  while (choise > 2 || choise < 1)
-  {
-    cout << "Наверное ты ошибся, повтори снова\n";
+    cout << "Привет, путник\nПрисядь у костра и расскажи о себе\n";
+    cout << "Ты впервые тут? (1 - новый персонаж, 2 - загрузить)\n";
+    unsigned short choise = 1;
+    //создать уникальную функцию для проверки условия выбора
     cin >> choise;
-  }
-  if (choise == 1)
-  {
-    cout << "Расскажи о своих навыках\n\t1 - Воин\n\t2 - Волшебник\n\t3 - Паладин\n";
-    
-    // тут уже будет вызвана ваша красивая функция
+    while (choise > 2 || choise < 1)
+    {
+        cout << "Наверное ты ошибся, повтори снова\n";
+        cin >> choise;
+    }
+    if (choise == 1)
+    {
+        cout << "Расскажи о своих навыках\n\t1 - Воин\n\t2 - Волшебник\n\t3 - Паладин\n";
+
+        // тут уже будет вызвана ваша красивая функция
+        cin >> choise;
+        while (choise > 3 || choise < 1)
+        {
+            cout << "Такого еще не было в наших краях\nНе мог бы ты повторить\n";
+            cin >> choise;
+        }
+
+
+        switch (choise)
+        {
+        case 1: {
+            player->Create(warrior);
+            delete wizard;
+            wizard = nullptr;
+            delete paladin;
+            paladin = nullptr;
+            break; }
+        case 2: {
+            player->Create(wizard);
+            delete warrior;
+            warrior = nullptr;
+            delete paladin;
+            paladin = nullptr;
+            cout << " " << endl;
+            break; }
+        case 3: {
+            player->Create(paladin);
+            delete warrior;
+            warrior = nullptr;
+            delete wizard;
+            wizard = nullptr;
+            break; }
+        }
+
+    }
+
+    else
+    {
+        player->Load(warrior);
+    }
+
+    cout << "сделаем остановку тут?\n\t1 - сохранить игру\n\t2 - продолжить\n";
     cin >> choise;
-    while (choise > 3 || choise < 1)
+    if (choise == 1)
     {
-      cout << "Такого еще не было в наших краях\nНе мог бы ты повторить\n";
-      cin >> choise;
+        if (warrior != nullptr) player->Save(warrior);
+        if (wizard != nullptr) player->Save(wizard);
+        if (paladin != nullptr) player->Save(paladin);
     }
-    
-    
-    switch (choise)
-    {
-    case 1: {
-      player->Create(warrior);
-      delete wizard;
-      wizard = nullptr;
-      delete paladin;
-      paladin = nullptr;
-      break; }
-    case 2: {
-      player->Create(wizard);
-      delete warrior;
-      warrior = nullptr;
-      delete paladin;
-      paladin = nullptr;
-      cout << " " << endl;
-      break; }
-    case 3: {
-      player->Create(paladin);
-      delete warrior;
-      warrior = nullptr;
-      delete wizard;
-      wizard = nullptr;
-      break; }
-    }
-    
-  }
-  
-  else
-  {
-      player->Load(warrior);
-  }
-
-  cout << "сделаем остановку тут?\n\t1 - сохранить игру\n\t2 - продолжить\n";
-  cin >> choise;
-  if (choise == 1)
-  {
-      if (warrior != nullptr) player->Save(warrior);
-      if (wizard != nullptr) player->Save(wizard);
-      if (paladin != nullptr) player->Save(paladin);
-  }
-  
 
 
-  return 0;
+
+    return 0;
 }
