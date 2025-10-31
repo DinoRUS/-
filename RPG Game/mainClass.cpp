d769ec538d7388a73670d32cce7945d92ecf6cc8
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2025-10-31 13:33:53 +0400

    Create mainClass.cpp

---
+++ RPG Game/mainClass.cpp
@@ -0,0 +1,66 @@
+#pragma once
+#include "mainClass.h"
+
+Npc::Npc string GetName()
+{
+    return name;
+}
+unsigned int GetHealth()
+{
+    return health;
+}
+float GetDamage()
+{
+    return damage;
+}
+unsigned int GetLvl()
+{
+    return lvl;
+}
+virtual void GetInfo() //метод класса
+{
+    cout << "Имя - " << name << endl;
+    cout << "Здоровье - " << health << endl;
+    cout << "Урон - " << damage << endl;
+}
+
+virtual void Create() {};
+virtual bool Save()
+{
+
+    ofstream saveSystem("save.bin", ios::binary);
+    if (saveSystem.is_open())
+    {
+        saveSystem.write(reinterpret_cast<const char*>(&name), sizeof(name));
+        saveSystem.write(reinterpret_cast<const char*>(&health), sizeof(health));
+        saveSystem.write(reinterpret_cast<const char*>(&damage), sizeof(damage));
+        saveSystem.write(reinterpret_cast<const char*>(&lvl), sizeof(lvl));
+        saveSystem.close();
+        return true;
+    }
+    else
+    {
+        cout << "сохранение не удалось" << endl;
+        return false;
+    }
+    saveSystem.close();
+};
+Npc Load()
+{
+    ifstream loadSystem("save.bin", ios::binary);
+    Npc npc; //временное хранилище для считывания данных из файла
+    if (loadSystem.is_open())
+    {
+        loadSystem.read(reinterpret_cast<char*>(&npc.name), sizeof(npc.name));
+        loadSystem.read(reinterpret_cast<char*>(&npc.health), sizeof(npc.health));
+        loadSystem.read(reinterpret_cast<char*>(&npc.damage), sizeof(npc.damage));
+        loadSystem.read(reinterpret_cast<char*>(&npc.lvl), sizeof(npc.lvl));
+    }
+    else
+    {
+        cout << "связь с базой нарушена\nПамять утерена" << endl;
+        return npc;
+    }
+    loadSystem.close();
+    return npc;
+
